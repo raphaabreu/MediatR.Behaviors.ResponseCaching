7cc02470a378eb300a354d0d9672f97ad9aacc24
Raphael Abreu <raphael.abreu@wappa.com.br>
2019-09-09 08:26:17 -0300

    Initial commit

---
+++ MediatR.Behaviors.ResponseCaching/ResponseCachingBehavior.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using App.Metrics;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace MediatR.Behaviors.ResponseCaching
+{
+    public class ResponseCachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly string _cacheKeyPrefix;
+        private readonly DistributedCacheEntryOptions _options;
+        private readonly IDistributedCache _cache;
+        private readonly IMetrics _metrics;
+        private readonly ILogger<ResponseCachingBehavior<TRequest, TResponse>> _logger;
+
+        public ResponseCachingBehavior(
+            string cacheKeyPrefix,
+            DistributedCacheEntryOptions options,
+            IDistributedCache cache,
+            IMetrics metrics,
+            ILogger<ResponseCachingBehavior<TRequest, TResponse>> logger
+        )
+        {
+            _cacheKeyPrefix = cacheKeyPrefix;
+            _options = options;
+            _cache = cache;
+            _metrics = metrics;
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var cacheKey = ComputeCacheKey(request);
+
+            _logger?.LogDebug("Checking distributed cache for {CacheKey}", cacheKey);
+            _metrics?.Measure.Meter.Mark(MetricsOptions.MEDIATOR_RESPONSE_CACHE_READ, new MetricTags("request", request.GetType().Name));
+
+            var cachedResponse = await _cache.GetStringAsync(cacheKey, cancellationToken);
+            if (cachedResponse != null)
+            {
+                try
+                {
+                    var cached = JsonConvert.DeserializeObject<TResponse>(cachedResponse);
+
+                    _logger?.LogInformation("Cached response found for {CacheKey}", cacheKey);
+
+                    return cached;
+                }
+                catch (Exception ex)
+                {
+                    _metrics?.Measure.Meter.Mark(MetricsOptions.MEDIATOR_RESPONSE_CACHE_READ_EXCEPTION, new MetricTags(new string[] { "request", "exception" }, new string[] { request.GetType().Name, ex.GetType().FullName }));
+                    _logger?.LogError(ex, "Failed to deserialize cached response for {CacheKey}", cacheKey);
+                }
+            }
+
+            _metrics?.Measure.Meter.Mark(MetricsOptions.MEDIATOR_RESPONSE_CACHE_READ_MISS, new MetricTags("request", request.GetType().Name));
+            _logger?.LogInformation("Fetching {CacheKey} from upstream", cacheKey);
+
+            var result = await next();
+
+            try
+            {
+                _metrics?.Measure.Meter.Mark(MetricsOptions.MEDIATOR_RESPONSE_CACHE_WRITE, new MetricTags("request", request.GetType().Name));
+                _logger?.LogDebug("Caching response for {CacheKey}", cacheKey);
+                var responseJson = JsonConvert.SerializeObject(result);
+                await _cache.SetStringAsync(cacheKey, responseJson, _options, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _metrics?.Measure.Meter.Mark(MetricsOptions.MEDIATOR_RESPONSE_CACHE_WRITE_EXCEPTION, new MetricTags(new string[] { "request", "exception" }, new string[] { request.GetType().Name, ex.GetType().FullName }));
+                _logger?.LogError(ex, "Failed to cache response for {CacheKey}", cacheKey);
+            }
+
+            return result;
+        }
+
+        private string ComputeCacheKey(TRequest request)
+        {
+            var requestJson = JsonConvert.SerializeObject(request);
+
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var textData = System.Text.Encoding.UTF8.GetBytes(requestJson);
+                var hash = md5.ComputeHash(textData);
+                return _cacheKeyPrefix + (new Guid(hash)).ToString("N");
+            }
+        }
+    }
+}
---
+++ MediatR.Behaviors.ResponseCaching/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using App.Metrics;
+using MediatR.Behaviors.ResponseCaching;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MediatR
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediatorResponseCaching<TRequest, TResponse>(
+            this IServiceCollection services,
+            string cacheKeyPrefix,
+            DistributedCacheEntryOptions options
+        ) where TRequest : IRequest<TResponse>
+        {
+            services.AddTransient<IPipelineBehavior<TRequest, TResponse>>(provider =>
+                new ResponseCachingBehavior<TRequest, TResponse>(
+                    cacheKeyPrefix,
+                    options,
+                    provider.GetRequiredService<IDistributedCache>(),
+                    provider.GetService<IMetrics>(),
+                    provider.GetService<ILogger<ResponseCachingBehavior<TRequest, TResponse>>>()
+                )
+            );
+
+            return services;
+        }
+
+        public static IServiceCollection AddMediatorResponseCaching<TRequest, TResponse>(
+            this IServiceCollection services,
+            string cacheKeyPrefix,
+            TimeSpan absoluteExpirationRelativeToNow
+        ) where TRequest : IRequest<TResponse>
+        {
+            services.AddTransient<IPipelineBehavior<TRequest, TResponse>>(provider =>
+                new ResponseCachingBehavior<TRequest, TResponse>(
+                    cacheKeyPrefix,
+                    new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow
+                    },
+                    provider.GetRequiredService<IDistributedCache>(),
+                    provider.GetService<IMetrics>(),
+                    provider.GetService<ILogger<ResponseCachingBehavior<TRequest, TResponse>>>()
+                )
+            );
+
+            return services;
+        }
+    }
+}
